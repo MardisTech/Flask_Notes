9c620ac272849b723caea16855ec2405c4600485
Alex Mardis <mardistech@gmail.com>
2023-08-08 13:18:44 -0400

    fixed bug added finishing touches

--- website/auth.py
+++ website/auth.py
@@ -39,25 +39,23 @@ def sign_up():
         password2 = request.form.get('password2')
 
         user = User.query.filter_by(email=email).first()
-
         if user:
-            flash('User already exists.', category='error')
+            flash('Email already exists.', category='error')
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
-            flash('First Name must be greater than 1 character.', category='error')
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords do not match', category='error')
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
-            flash('Password must be atlease 7 characters', category='error')
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            #add user to database
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
-
+    return render_template("sign_up.html", user=current_user)
